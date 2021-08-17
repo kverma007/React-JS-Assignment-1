e7b6f8155cef91907b513f5686cdfdb34e50e404
kuber verma <70593413+kverma007@users.noreply.github.com>
2021-08-17 19:13:14 +0530

    Add files via upload

---
+++ Assignment1/src/Header.js
@@ -0,0 +1,22 @@
+
+function Header(){
+
+    let name="Saurabh";
+
+    return (
+
+        <div>
+
+            <h1>Hello {name}</h1>
+
+           
+
+        </div>
+
+        
+    )
+
+}
+
+
+export default Header;
